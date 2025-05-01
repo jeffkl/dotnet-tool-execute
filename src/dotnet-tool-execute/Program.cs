b338c0c06229ca2210ec28bfdba73d627514f0aa
Jeff Kluge <jeffkl@microsoft.com>
2025-05-01 10:13:14 -0700

    Cleanup

--- src/dotnet-tool-execute/Program.cs
+++ src/dotnet-tool-execute/Program.cs
@@ -4,114 +4,114 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
-namespace DotnetToolExecute
+namespace DotnetToolExecute;
+
+public static class Program
 {
-    public static class Program
+    public static int Main(string[] args)
     {
-        public static int Main(string[] args)
+        if (string.Equals(Environment.GetEnvironmentVariable("DEBUG_DTX"), bool.TrueString, StringComparison.OrdinalIgnoreCase))
         {
-            if (string.Equals(Environment.GetEnvironmentVariable("DEBUG_DTX"), bool.TrueString, StringComparison.OrdinalIgnoreCase))
-            {
-                Debugger.Launch();
-            }
-
-            (string? toolName, List<string> args1, List<string> args2) = ParseArgs(args);
-
-            if (toolName == null)
-            {
-                return PrintUsage();
-            }
-
-            (int installToolExitCode, FileInfo? toolExecutable) = InstallTool(toolName, args1);
-
-            if (installToolExitCode != 0 || toolExecutable == null)
-            {
-                return installToolExitCode;
-            }
-
-            return RunTool(toolExecutable, args2);
+            Debugger.Launch();
         }
 
-        private static (int, FileInfo?) InstallTool(string toolName, List<string> args1)
+        (string? toolName, List<string> args1, List<string> args2) = ParseArgs(args);
+
+        if (toolName == null)
         {
-            DirectoryInfo toolPath = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "dtx", toolName));
-
-            using Process process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "dotnet",
-                    Arguments = string.Join(" ", ["tool", "install", toolName, "--tool-path", toolPath, .. args1]),
-                },
-            };
-
-            process.Start();
-
-            process.WaitForExit();
-
-            return (process.ExitCode, process.ExitCode == 0 ? toolPath.EnumerateFiles().FirstOrDefault() : null);
+            return PrintUsage();
         }
 
-        private static (string? toolName, List<string> args1, List<string> args2) ParseArgs(string[] args)
+        (int installToolExitCode, FileInfo? toolExecutable) = InstallTool(toolName, args1);
+
+        if (installToolExitCode != 0 || toolExecutable == null)
         {
-            var args1 = new List<string>();
-            var args2 = new List<string>();
-            bool firstArgs = true;
+            return installToolExitCode;
+        }
 
-            string? toolName = null;
+        return RunTool(toolExecutable, args2);
+    }
 
-            for (int i = 0; i < args.Length; i++)
+    private static (int, FileInfo?) InstallTool(string toolName, List<string> args1)
+    {
+        DirectoryInfo toolPath = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "dtx", toolName));
+
+        using Process process = new Process
+        {
+            StartInfo = new ProcessStartInfo
             {
-                string? arg = args[i];
-                if (i == 0)
-                {
-                    toolName = arg;
-                    continue;
-                }
+                FileName = "dotnet",
+                Arguments = string.Join(" ", ["tool", "install", toolName, "--tool-path", toolPath, .. args1]),
+            },
+        };
 
-                if (arg == "--")
-                {
-                    firstArgs = false;
-                    continue;
-                }
+        process.Start();
 
-                if (firstArgs)
-                {
-                    switch (arg)
-                    {
-                        case "--global":
-                        case "-g":
-                        case "--local":
-                            continue;
+        process.WaitForExit();
 
-                        case "--help":
-                        case "-h":
-                        case "-?":
-                            toolName = null;
-                            continue;
+        return (process.ExitCode, process.ExitCode == 0 ? toolPath.EnumerateFiles().FirstOrDefault() : null);
+    }
 
-                        case "--tool-path":
-                        case "--tool-manifest":
-                        case "--create-manifest-if-needed":
-                            i++;
-                            continue;
-                        default:
-                            args1.Add(arg);
-                            break;
-                    }
-                }
-                else
-                {
-                    args2.Add(arg);
-                }
+    private static (string? toolName, List<string> args1, List<string> args2) ParseArgs(string[] args)
+    {
+        var args1 = new List<string>();
+        var args2 = new List<string>();
+        bool firstArgs = true;
+
+        string? toolName = null;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            string? arg = args[i];
+            if (i == 0)
+            {
+                toolName = arg;
+                continue;
             }
 
-            return (toolName, args1, args2);
+            if (arg == "--")
+            {
+                firstArgs = false;
+                continue;
+            }
+
+            if (firstArgs)
+            {
+                switch (arg)
+                {
+                    case "--global":
+                    case "-g":
+                    case "--local":
+                        continue;
+
+                    case "--help":
+                    case "-h":
+                    case "-?":
+                        toolName = null;
+                        continue;
+
+                    case "--tool-path":
+                    case "--tool-manifest":
+                    case "--create-manifest-if-needed":
+                        i++;
+                        continue;
+                    default:
+                        args1.Add(arg);
+                        break;
+                }
+            }
+            else
+            {
+                args2.Add(arg);
+            }
         }
 
-        private static int PrintUsage()
-        {
-            Console.WriteLine(@"Description:
+        return (toolName, args1, args2);
+    }
+
+    private static int PrintUsage()
+    {
+        Console.WriteLine(@"Description:
   Install and run a .NET tool.
 
 Usage:
@@ -137,27 +137,26 @@ Options:
   --allow-roll-forward         Allow a .NET tool to roll forward to newer versions of the .NET runtime if the runtime it targets isn't installed.
   -?, -h, --help               Show command line help.");
 
-            return 0;
-        }
+        return 0;
+    }
 
-        private static int RunTool(FileInfo toolExecutable, List<string> args2)
+    private static int RunTool(FileInfo toolExecutable, List<string> args2)
+    {
+        using Process process = new Process
         {
-            using Process process = new Process
+            StartInfo = new ProcessStartInfo
             {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = toolExecutable.FullName,
-                    Arguments = string.Join(" ", args2),
-                },
-            };
+                FileName = toolExecutable.FullName,
+                Arguments = string.Join(" ", args2),
+            },
+        };
 
-            process.StartInfo.EnvironmentVariables["DOTNET_ROLL_FORWARD"] = "LatestMajor";
+        process.StartInfo.EnvironmentVariables["DOTNET_ROLL_FORWARD"] = "LatestMajor";
 
-            process.Start();
+        process.Start();
 
-            process.WaitForExit();
+        process.WaitForExit();
 
-            return process.ExitCode;
-        }
+        return process.ExitCode;
     }
 }
